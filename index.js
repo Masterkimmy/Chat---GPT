d71b0e5803b74b59485eb0d2dbc1632b00007079
Master Kimmy <hakeemkolawole62@gmail.com>
2024-04-18 12:32:20 +0100

    fixed error

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function showSideBar() {
     const navlinks = document.querySelector('nav .navlinks')
     const overlay = document.querySelector('.overlay')
-    navlinks.style.display = "block"
     overlay.style.display = "block"
+    navlinks.style.left = "0px"
 }
 function hideSideBar() {
     const navlinks = document.querySelector('nav .navlinks')
     const overlay = document.querySelector('.overlay')
-    navlinks.style.display = "none"
     overlay.style.display = "none"
-}   
+    navlinks.style.left = "100%"
+}  
